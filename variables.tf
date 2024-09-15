7c9bb9aeb29dbec91b13243a91a54975b4e30f5d
Jason Henrique <jhmr.jason@outlook.com>
2024-09-15 10:00:55 -0300

    Aula 03

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "service_name" {}
 
+variable "region" {}
+
 variable "cluster_name" {}
 
 variable "vpc_id" {}
@@ -12,4 +14,25 @@ variable "service_cpu" {}
 
 variable "service_memory" {}
 
+variable "service_task_execution_role" {}
+
+variable "service_healthcheck" {
+  type = map
+}
+
+variable "service_launch_type" {}
+
+variable "service_task_count" {}
+
+variable "service_hosts" {}
+
 variable "service_listener" {}
+
+variable "environment_variables" {
+  type = list(any)
+}
+
+variable "capabilities" {
+  type = list(any)
+}
+
