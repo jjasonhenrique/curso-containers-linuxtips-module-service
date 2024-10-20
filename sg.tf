2f9d08614d3bbe329d1c27630a9ff609f94608f7
Jason Henrique <jhmr.jason@outlook.com>
2024-10-20 17:16:28 -0300

    Update Module aula 09

--- sg.tf
+++ sg.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 65635
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
