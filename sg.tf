b57207b6f9067b7c175f4ecf89007703e075014a
Jason Henrique <jhmr.jason@outlook.com>
2024-10-20 17:34:40 -0300

    Update Module

--- sg.tf
+++ sg.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "main" {
 
   ingress {
     from_port   = 0
-    to_port     = 65635
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
